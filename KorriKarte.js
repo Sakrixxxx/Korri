eae3a75e09d9a1b88abf045f435bf4a7352df11a
Sakrixxxx <149011085+Sakrixxxx@users.noreply.github.com>
2023-10-27 21:26:28 +0200

    Update KorriKarte.js

--- KorriKarte.js
+++ KorriKarte.js
@@ -52,5 +52,3 @@ MapSdk.lines.push({x1: 604.5, y1: 369.5, x2: 604.5, y2: 379.5, styling: {main: {
 MapSdk.lines.push({x1: 389.5, y1: 378.5, x2: 379.5, y2: 378.5, styling: {main: {"strokeStyle": "#FFFF00","lineWidth": 2}, mini: {"strokeStyle": "yellow","lineWidth": 2}}, drawOnMini: true, drawOnMap: true});
 MapSdk.lines.push({x1: 379.5, y1: 378.5, x2: 379.5, y2: 369.5, styling: {main: {"strokeStyle": "#FFFF00","lineWidth": 2}, mini: {"strokeStyle": "yellow","lineWidth": 2}}, drawOnMini: true, drawOnMap: true});
 MapSdk.mapOverlay.reload();});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
